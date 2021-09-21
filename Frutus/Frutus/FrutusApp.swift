299d94cf2b669683b00bbbad5e2f3a505342365f
Taha Noor <taha.noor@phaedrasolutions.com>
2021-09-21 14:54:42 +0500

    Initial setup

---
+++ Frutus/Frutus/FrutusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrutusApp.swift
+//  Frutus
+//
+//  Created by Macbook Pro on 21/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct FrutusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
