608c6539660c06e8f689b70755c614ed63d526de
Taha Noor <taha.noor@phaedrasolutions.com>
2021-09-21 15:22:56 +0500

    First Setup

---
+++ Frutus/Frutus/View/FruitCardView.swift
@@ -0,0 +1,30 @@
+//
+//  FruitCardView.swift
+//  Frutus
+//
+//  Created by Macbook Pro on 21/09/2021.
+//
+
+import SwiftUI
+
+struct FruitCardView: View {
+    //MARK:- Properties
+    
+        
+   //MARK:- Body
+    var body: some View {
+    
+        
+        Text("Blueberryy")
+    }
+}
+
+//MARK:- Properties
+
+struct FruitCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitCardView()
+            .previewLayout(.fixed(width: 320, height: 640))
+        
+    }
+}
